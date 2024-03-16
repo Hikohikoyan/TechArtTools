c9255558a5fe711cc0d762e3454497a0c3b7a08f
Hiko <huanghongyan324@gmail.com>
2024-03-16 17:46:39 +0800

    Update ShaderConvertor: Auto-Generate Unity Shader from UE Material

---
+++ ShaderCodeConvertor/GlobalSettings.py
@@ -0,0 +1,67 @@
+# 导入必要的库
+import re
+
+log_file = "Log.txt"
+log = ""
+
+def write_log(func,s):
+    global log 
+    log += func  + " " + "\n    " + s + ""
+    return log
+
+def make_comment(desc):
+    return "// "+desc
+
+
+#判断所表达的浮点数数据类型是float|float3|float4|未知
+def identify_float_type(s):
+    # 去掉字符串中的字母
+    s = re.sub('[^0-9,.]', '', s)
+    # 统计字符串中“,”的个数
+    num_commas = s.count(',')
+    isInt = s.count('.')<1
+    # 如果字符串中没有“,”，则为float类型
+    if isInt:
+        return "int"
+    elif num_commas == 0:
+        return "float"
+    elif num_commas == 2:
+        return "float3"
+    elif num_commas == 3:
+        return "float4"
+    else:
+        return "未知类型"
+
+#制作缩进数量 同’tab'键
+def make_indentation(num):
+    res = []
+    space = "    "
+    for i in range(0,num): 
+        res.append(space)
+    return "".join(res)
+
+def clean_value(v):
+    val_type = identify_float_type(v)
+    s = ""
+    if val_type == "int":
+        s = v.replace(".","")  # 匹配小数点前面的数字，如果是整数，则去掉小数点
+    else:
+        s = v.replace("(","").replace(")","")
+        if val_type == "float":
+            s = re.sub('[^0-9,.]', '', v)
+            s = re.sub(r"(\.\d*?)0+$", r"\1",s)+"0"
+        else :
+            s = re.sub('[^0-9,.]', '', v)
+            t = []
+            num = 0
+            for i in s.split(","):
+                num += 1
+                i = re.sub(r"(\.\d*?)0+$", r"\1",i)+"0"
+                if num < len(s.split(",")):
+                    t.append(i + ",")
+                else:
+                    t.append(i)
+            s = "".join(t)
+    #debug
+    write_log("\nclean_value","val_type " + val_type + "  return " + s)
+    return s
--- ShaderCodeConvertor/UnityShaderGenerator.py
+++ ShaderCodeConvertor/UnityShaderGenerator.py
@@ -1,39 +1,25 @@
 # 导入必要的库
-import re
-import json
-import Convertor
+import GlobalSettings as gls
+import UnrealMaterialGraphDict as ue_dict
+import UnityShaderConfig as unity_config
+import UnrealMaterialGraphFilter as graph_nodes
 # 定义输入和输出文件路径
-input_file = "output.txt"
-output_cginc = "gen.cginc"
-output_shader = "gen.shader"
+input_file = "collection_for_unity_shader.txt" 
+#input("请输入For Unity的TXT 路径：")
+output_cginc = unity_config.cginc
+output_shader = unity_config.shader
 
 log_file = "Log.txt"
 log = ""
 
 
 # 定义hlsl shader code和cgsl的对应关系
-cginc_dict = {
-    "Texture":"sampler2D",
-    "float":"half",
-    "int" :"half",
-    "float3":"half3",
-    "float4":"half4",
-}
+cginc_dict = unity_config.cginc_dict
 
-shader_dict = {
-    "Texture" : ', 2D) =',
-    "int" : ", Range(0,1)) = ",
-    "float" : ", Range(0,1)) = ",
-    "float3" : ", Vector) =",
-    "float4":", Vector) =",
-    "Color":", Color) = (1,1,1)"
-}
+shader_dict = unity_config.shader_dict
 
 #texture_type
-texture_type ={
-    "SAMPLERTYPE_Normal":"bump",
-    "SAMPLERTYPE_LinearColor":"black",
-}
+texture_type =unity_config.texture_type
 
 # 读取输入文件内容
 #with open(input_file, "r") as f:
@@ -41,66 +27,16 @@ texture_type ={
 with open(input_file, "r", encoding="utf-8") as f:
     content = f.read()
 
-def write_log(func,s):
-    global log 
-    log += func  + " " + "\n    " + s + ""
-    return log
+
 def make_param(t,n,config):
     #input type name
     res = ""
-    res = Convertor.replace_content(t,config) + " " + n
+    res = graph_nodes.replace_content(t,config) + " " + n
     return res
 
-def make_comment(desc):
-    return "// "+desc
 
-
-def clean_value(v):
-    val_type = identify_float_type(v)
-    s = ""
-    if val_type == "int":
-        s = v.replace(".","")  # 匹配小数点前面的数字，如果是整数，则去掉小数点
-    else:
-        s = v.replace("(","").replace(")","")
-        if val_type == "float":
-            s = re.sub('[^0-9,.]', '', v)
-            s = re.sub(r"(\.\d*?)0+$", r"\1",s)+"0"
-        else :
-            s = re.sub('[^0-9,.]', '', v)
-            t = []
-            num = 0
-            for i in s.split(","):
-                num += 1
-                i = re.sub(r"(\.\d*?)0+$", r"\1",i)+"0"
-                if num < len(s.split(",")):
-                    t.append(i + ",")
-                else:
-                    t.append(i)
-            s = "".join(t)
-    #debug
-    write_log("\nclean_value","val_type " + val_type + "  return " + s)
-    return s
-
-def identify_float_type(s):
-    # 去掉字符串中的字母
-    s = re.sub('[^0-9,.]', '', s)
-    # 统计字符串中“,”的个数
-    num_commas = s.count(',')
-    isInt = s.count('.')<1
-    # 如果字符串中没有“,”，则为float类型
-    if isInt:
-        return "int"
-    elif num_commas == 0:
-        return "float"
-    elif num_commas == 2:
-        return "float3"
-    elif num_commas == 3:
-        return "float4"
-    else:
-        return "未知类型"
-
+#解析数据包
 def decode_input(item):
-
     pack = {
         "param_name" : "",
         "param_type" :"",
@@ -120,45 +56,46 @@ def decode_input(item):
         info_type = "".join(info.split(":")[0]).replace("}","").replace("{","")
         info_val = info.replace(info_type,"").replace(":","").replace('"',"")
         # #debug
-        # write_log("" ,"info_type:  " + info_type)
-        # write_log("" ,"info_val:  " + info_val)
+        # gls.write_log("" ,"info_type:  " + info_type)
+        # gls.write_log("" ,"info_val:  " + info_val)
 
         # get ParameterName
-        if (Convertor.is_key_in_line(info_type,"ParameterName")):
+        if (graph_nodes.is_key_in_line(info_type,"ParameterName")):
             pack["param_name"] = info_val
-            if Convertor.is_key_in_line(info_val,"Tex"):
+            if graph_nodes.is_key_in_line(info_val,"Tex"):
                 pack["isNotTexture"] = True
 
         # get ParameterGroup
-        if (Convertor.is_key_in_line(info_type,"Group")):
+        if (graph_nodes.is_key_in_line(info_type,"Group")):
             pack["param_group"] = info_val
-            if Convertor.is_key_in_line(info_val,"贴图"):
+            if graph_nodes.is_key_in_line(info_val,"贴图"):
                 pack["isNotTexture"] = True
 
         # get ParameterDesc
-        if (Convertor.is_key_in_line(info_type,"Desc")):
+        if (graph_nodes.is_key_in_line(info_type,"Desc")):
             pack["param_desc"] = info_val
 
         # get ParameterValue
-        if (Convertor.is_key_in_line(info_type,"DefaultValue")):
-            pack["param_val"] = clean_value(info_val)
+        if (graph_nodes.is_key_in_line(info_type,"DefaultValue")):
+            pack["param_val"] = gls.clean_value(info_val)
 
         # get TextureValue
-        if (Convertor.is_key_in_line(info_type,"SamplerType")):
+        if (graph_nodes.is_key_in_line(info_type,"SamplerType")):
             pack["isNotTexture"] = True
             pack["texture_val"] = info_val
 
     #debug
-    # write_log("" ,"param_val:" +pack["param_val"])
-    # write_log("" ,"param_name:" +pack["param_name"])
-    # write_log("decode_input" ,"param_desc:" +pack["param_desc"])
+    # gls.write_log("" ,"param_val:" +pack["param_val"])
+    # gls.write_log("" ,"param_name:" +pack["param_name"])
+    # gls.write_log("decode_input" ,"param_desc:" +pack["param_desc"])
     if pack["isNotTexture"] :
         #贴图参数
         pack["param_type"] = "Texture"
     else:
-        pack["param_type"] = identify_float_type(pack["param_val"])
+        pack["param_type"] = gls.identify_float_type(pack["param_val"])
     return pack
-    
+
+#制作cginc文件
 def write_cginc(content):
     params = content.replace("\n","").replace(" ","").split("}")
     res = []
@@ -171,13 +108,19 @@ def write_cginc(content):
             continue
         t = make_param(item["param_type"],item["param_name"],cginc_dict).replace(";","") +";"
         res.append(t)
-        d = make_comment( item["param_desc"])+"\n"
+        desc = item["param_desc"]
+        if desc == "":
+            desc = item["param_group"]
+        d = gls.make_comment(desc )+"\n"
         res.append(d)
 
     #print(type(json_str))
     return "\n".join(res)  # 将新的列表转换成字符串，每行以换行符连接起来
 
-def write_shader(content):
+
+
+#制作shader文件
+def write_shader(content,num):
 
     params = content.replace("\n","").replace(" ","").split("}")
     res = []
@@ -189,20 +132,20 @@ def write_shader(content):
         if item == None or checkstatus:
             continue
         name = item["param_name"]
-        val = clean_value(item["param_val"])
+        val = gls.clean_value(item["param_val"])
         desc = item["param_desc"]
         type_str = item["param_type"]
 
-        write_log("\nwrite_shader","name " + name + " val " + val +" type_str " + type_str)
-        if identify_float_type(val) != "float" and Convertor.is_key_in_line(name,"Color") and isNotTex:
+        gls.write_log("\nwrite_shader","name " + name + " val " + val +" type_str " + type_str)
+        if gls.identify_float_type(val) != "float" and graph_nodes.is_key_in_line(name,"Color") and isNotTex:
             type_str = "Color"
             val = "("+val+")"
         line = "_" + name + '("' + desc + " " + name + '"'+ make_param(type_str,"",shader_dict).replace(";","")
         if type_str == "Texture":
-            write_log("\nwrite_shader","texture_val " + item["texture_val"])
+            gls.write_log("\nwrite_shader","texture_val " + item["texture_val"])
             line += '"' 
             if( item["texture_val"] != ""):
-                line += Convertor.replace_content(item["texture_val"],texture_type)
+                line += graph_nodes.replace_content(item["texture_val"],texture_type)
             else:
                 line += "white"
             line += '" {' + '}'
@@ -210,24 +153,51 @@ def write_shader(content):
             range_str = "Range(0," + val +")"
             line.replace(" Range(0,1)",range_str)
             line += " " + val
-        res.append( line )
-        write_log("\nwrite_shader",line)
-        res.append(make_comment( desc)+"\n")
+        res.append( gls.make_indentation(2)  + line )
+        gls.write_log("\nwrite_shader",line)
+        if desc == "":
+            desc = item["param_group"]
+        res.append( gls.make_indentation(2)  + gls.make_comment( desc)+"\n")
         
     #print(type(json_str))
-    return "\n".join(res)  # 将新的列表转换成字符串，每行以换行符连接起来
+    maincontainer = "Shader 'Sample/YourShaderName'\n{\n"
+    prop_str = gls.make_indentation(1) + "Properties\n" + gls.make_indentation(1) +"{\n"
+    prop_str += "\n".join(res) +"\n" + gls.make_indentation(1) +  "}\n"
 
+    sub_str = "\n" +  gls.make_indentation(1) + "SubShader\n" +  gls.make_indentation(1) +"{"
 
-result_cginc = write_cginc(content)
-result_shader = write_shader(content)
+    #subshader tags
+    tag_str = unity_config.make_shader_tags(1,{"Queue":"Geometry","RenderType":"Opaque"},"")
 
-#将转换后的内容写入输出文件
+    pass1_str = unity_config.make_subshader_pass(2,{"LightMode":"ForwardBase"},"/gen.cginc",True,True,True)
+
+    sub = []
+    for i in range(0,num):
+        if(i == 0):
+            pass2_str = unity_config.make_subshader_pass(2,{"LightMode":"ForwardBase"},"",False,False,False)
+            s = sub_str + tag_str + pass1_str + pass2_str + gls.make_indentation(1) +  "}\n" 
+        else:
+            pass2_str = unity_config.make_subshader_pass(2,{"LightMode":"ForwardBase"},"",False,False,False)
+            s = sub_str + tag_str + pass2_str  + gls.make_indentation(1) +  "}\n" 
+        sub.append(s)
+    maincontainer += prop_str + "".join(sub) +"\n}"
+    return  maincontainer # 将新的列表转换成字符串，每行以换行符连接起来
+
+#写UnityShader Cginc
+subshader_num = 2 #int(input("Shader文件包含几个subshader:"))
+#Shader文件包含几个subshader
+
+result_cginc = write_cginc(content.split(ue_dict.split_signal)[0])
+result_shader = write_shader(content.split(ue_dict.split_signal)[0],subshader_num)
+
+#将转换后的内容写入输出文件output_cginc
 with open(output_cginc, "w", encoding="utf-8") as f:
     f.write(result_cginc)
 
-#将转换后的内容写入输出文件
+#将转换后的内容写入输出文件output_shader
 with open(output_shader, "w", encoding="utf-8") as f:
     f.write(result_shader)
 
+#输出Debug Log
 with open(log_file, "w", encoding="utf-8") as f:
     f.write(log)
